8a2c155e0996f6260ab1dcef2cf4f599029e991f
Prince Raghuvanshi <adityaraghuvanshi159@gmail.com>
2024-09-01 10:42:49 +0530

    removed one bug

--- src/components/DisplayAllTask.js
+++ src/components/DisplayAllTask.js
@@ -3,13 +3,13 @@ import { useSelector } from 'react-redux'
 import Row from './Row';
 
 const DisplayAllTask = () => {
-    const data=useSelector(store=>store.task.lists);
+    const data=useSelector((store)=>store.task.lists);
     // console.log(data);
   return (
         <div>
             {
                 data.map((item,index)=>{
-                    return <Row key={index} index={index} item={item}/>
+                    return <Row key={index} index={index+1} item={item}/>
                 })
             }
         </div>
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,6 +4,7 @@ import { pushTask } from '../utils/taskSlice';
 import DisplayAllTask from './DisplayAllTask';
 
 const MainContainer = () => {
+    const [inputValue,setInputValue]=useState("");
     const [data,setData]= useState("");
     const dispatch = useDispatch();
     const handleClick=()=>{
@@ -11,13 +12,13 @@ const MainContainer = () => {
         // console.log(Date());
         const date=Date();
         dispatch(pushTask({data,date}));
-
+        setInputValue("");
     }
   return (
     <div className='mt-6 bg-white h-screen w-8/12 mx-auto p-10'>
       <h1 className='text-center font-bold text-4xl font-sans mb-4'>Add todo works</h1>
       <div className='flex '>
-        <input type="text" onChange={e=>setData(e.target.value)} className='w-3/4 px-5 border-2 border-black' placeholder='Write your todo task here..'/>
+        <input type="text" value={inputValue} onChange={e=>setInputValue(e.target.value)} className='w-3/4 px-5 border-2 border-black' placeholder='Write your todo task here..'/>
         <button onClick={handleClick} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium text-sm w-1/2 px-5 py-2 mx-4 text-center mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">ADD</button>
       </div>
       <DisplayAllTask/>
